d0e6eafe30f3bee93570edbe93532132c2684bac
sanmi <sossesam@gmail.com>
2024-11-03 23:22:55 +0100

    all

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,6 +5,7 @@ BasicCaching = __import__('base_caching').BaseCaching
 class BasicCache(BasicCaching):
     """ 0-main """
     def __init__(self):
+        """ 0-main """
         super().__init__()
 
     def put(self, key, item):
