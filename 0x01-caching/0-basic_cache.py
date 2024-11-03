d1495c592413330b24de59b1ac9236fcacba8aed
sanmi <sossesam@gmail.com>
2024-11-03 23:26:53 +0100

    all

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,6 +3,7 @@
 
 BasicCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BasicCaching):
     """ 0-main is working"""
     def __init__(self):
@@ -11,14 +12,14 @@ class BasicCache(BasicCaching):
 
     def put(self, key, item):
         """ 0-main is working"""
-        if key == None or item == None:
+        if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """ 0-main is working"""
-        if self.cache_data.get(key) == None or key == None:
+        if self.cache_data.get(key) is None or key is None:
             return None
         else:
             return self.cache_data[key]
