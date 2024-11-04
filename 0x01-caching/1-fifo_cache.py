c7a8f1dbfb936452631f2c88e277529bce138bc6
sanmi <sossesam@gmail.com>
2024-11-04 02:05:57 +0100

    all

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,19 +3,26 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """ 0-main is working"""
-    def __init__(self):
-        """ 0-main is working"""
+    def __init__(self, *args, **kwargs):
         super().__init__()
+        self.memlist = []
 
     def put(self, key, item):
         """ 0-main is working"""
+
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
+            if len(self.memlist) > BaseCaching.MAX_ITEMS:
+                removed_key = self.memlist.pop(0)
+                del self.cache_data[removed_key]
+                print(f"DISCARD: {removed_key}")
+                self.memlist.append(key)
+            else:
+                self.memlist.append(key)
 
     def get(self, key):
         """ 0-main is working"""
