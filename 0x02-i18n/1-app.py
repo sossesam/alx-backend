64ae29c169ade5e1b41d398d7413ac196e8456d7
sanmi <sossesam@gmail.com>
2024-11-09 07:27:06 +0100

    all

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,17 +4,17 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
+    """ this is a documentation """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
-
-
-
 
 
 @app.route("/")
