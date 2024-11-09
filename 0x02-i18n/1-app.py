927a5566e0fd5a1e1fbaad5377195932534dc24a
sanmi <sossesam@gmail.com>
2024-11-09 07:25:32 +0100

    all

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" this is a documentation """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+
+
+
+@app.route("/")
+def home():
+    """ this is a documentation """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
