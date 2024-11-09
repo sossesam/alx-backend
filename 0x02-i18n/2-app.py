8523ccdeb6b7ab4029d402f654a46eea9b836713
sanmi <sossesam@gmail.com>
2024-11-09 08:00:39 +0100

    all

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ this is a documentation """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
 
 
 
@@ -17,11 +18,14 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route("/")
 def home():
     """ this is a documentation """
-    return render_template("1-index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
