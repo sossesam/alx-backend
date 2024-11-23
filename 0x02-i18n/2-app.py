bdeb0ce90328ae7a747689907acfa113ca6c1efa
sanmi <sossesam@gmail.com>
2024-11-23 18:44:24 +0100

    all

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route("/")
 def home():
     """ this is a documentation """
-    return render_template("0-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
